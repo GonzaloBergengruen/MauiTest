2e44700b6cb5894156c7f2c0af4318f4b257df85
GonzaloBergengruen <bergengruengonzalo@gmail.com>
2024-11-13 14:12:49 -0300

    Notificacion

---
+++ Notificacion/Notificacion/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Plugin.LocalNotification;
+
+namespace Notificacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            var request = new NotificationRequest
+            {
+                NotificationId = 1,
+                Title = "Intento 1",
+                Subtitle = "Hola",
+                Description = "Texto de ejemplo",
+                BadgeNumber = 23,
+                Schedule = new NotificationRequestSchedule
+                {
+                    NotifyTime = DateTime.Now.AddSeconds(5),
+                }
+            };
+            LocalNotificationCenter.Current.Show(request);
+        }
+    }
+
+}
