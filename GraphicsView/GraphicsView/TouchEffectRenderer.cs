332f432a8db0967920208f759cccd292edf7f8de
GonzaloBergengruen <bergengruengonzalo@gmail.com>
2025-02-23 16:27:31 -0300

    IOS agregado

--- GraphicsView/GraphicsView/TouchEffectRenderer.cs
+++ GraphicsView/GraphicsView/TouchEffectRenderer.cs
@@ -46,4 +46,91 @@ namespace GraphicsView
         }
     }
 }
+
+#elif IOS
+
+using UIKit;
+using Microsoft.Maui.Controls.Platform;
+using GraphicsView;
+using CoreGraphics;
+using Foundation;
+
+[assembly: ResolutionGroupName("GraphicsView")]
+[assembly: ExportEffect(typeof(TouchEffectRenderer), "TouchEffect")]
+
+namespace GraphicsView
+{
+    public class TouchEffectRenderer : PlatformEffect
+    {
+        private UIView _view;
+        private TouchEffect _effect;
+        private UITapGestureRecognizer _tapGesture;
+        private UIPanGestureRecognizer _panGesture;
+
+        protected override void OnAttached()
+        {
+            _view = (UIView)(Control ?? Container);
+
+            if (_view != null)
+            {
+                _effect = (TouchEffect)Element.Effects.FirstOrDefault(e => e is TouchEffect);
+
+                 //Agregar gestos para manejar el toque y el arrastre
+                _tapGesture = new UITapGestureRecognizer(OnTap);
+                _panGesture = new UIPanGestureRecognizer(OnPan);
+
+                _view.UserInteractionEnabled = true;
+                _view.AddGestureRecognizer(_tapGesture);
+                _view.AddGestureRecognizer(_panGesture);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            if (_view != null)
+            {
+                if (_tapGesture != null)
+                {
+                    _view.RemoveGestureRecognizer(_tapGesture);
+                    _tapGesture.Dispose();
+                    _tapGesture = null;
+                }
+
+                if (_panGesture != null)
+                {
+                    _view.RemoveGestureRecognizer(_panGesture);
+                    _panGesture.Dispose();
+                    _panGesture = null;
+                }
+            }
+        }
+
+        private void OnTap(UITapGestureRecognizer recognizer)
+        {
+            if (_effect != null)
+            {
+                var touchPoint = recognizer.LocationInView(_view);
+                _effect.OnTouchAction(new TouchActionEventArgs(TouchActionType.Pressed, new Point(touchPoint.X, touchPoint.Y)));
+                _effect.OnTouchAction(new TouchActionEventArgs(TouchActionType.Released, new Point(touchPoint.X, touchPoint.Y)));
+            }
+        }
+
+        private void OnPan(UIPanGestureRecognizer recognizer)
+        {
+            if (_effect != null)
+            {
+                var touchPoint = recognizer.LocationInView(_view);
+                TouchActionType actionType = recognizer.State switch
+                {
+                    UIGestureRecognizerState.Began => TouchActionType.Pressed,
+                    UIGestureRecognizerState.Changed => TouchActionType.Moved,
+                    UIGestureRecognizerState.Ended => TouchActionType.Released,
+                    _ => TouchActionType.Cancelled
+                };
+
+                _effect.OnTouchAction(new TouchActionEventArgs(actionType, new Point(touchPoint.X, touchPoint.Y)));
+            }
+        }
+    }
+}
 #endif
--- GraphicsView/GraphicsView/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/GraphicsView.AssemblyInfo.cs
+++ GraphicsView/GraphicsView/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/GraphicsView.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphicsView")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7d990eb371ff78f24fea8e02d83cff1e867f2c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ff9da8739e4fce6cd0969eb848d5072ea1bf822d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphicsView")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphicsView")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
